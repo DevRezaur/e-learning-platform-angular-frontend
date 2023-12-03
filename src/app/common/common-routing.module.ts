e0441b7c50a304e0ffc12c61d8f5198a761527c4
Rezaur Rahman <rezaur.official@gmail.com>
2023-12-03 19:40:16 +0600

    CommonModule added

---
+++ src/app/common/common-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePageComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class CommonRoutingModule {}
---
+++ src/app/common/common.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+import { HomePageComponent } from './home-page/home-page.component';
+
+@NgModule({
+  declarations: [HomePageComponent],
+  imports: [],
+  exports: [HomePageComponent],
+})
+export class CommonModule {}
