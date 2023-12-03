800e7d1df238e52f98a7bba4da9ed758a3103d55
Rezaur Rahman <rezaur.official@gmail.com>
2023-12-03 18:47:32 +0600

    Added sidebar menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,19 @@ export class AppComponent {
   title = 'e-learning-platform-angular-frontend';
   showSidebar: boolean = false;
   isLoggedIn: boolean = true;
+  userMenu: string[] = [
+    'Home',
+    'Learning Dashboard',
+    'Notifications',
+    'Manage Profile',
+  ];
+  adminMenu: string[] = [
+    'Home',
+    'Admin Dashboard',
+    'Notifications',
+    'Manage Users',
+    'Manage Profile',
+  ];
+
+  activeMenu: string[] = this.userMenu;
 }
