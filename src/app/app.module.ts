8b4938bcea37130cdb71d5e0fadfb6b8d82e3651
Rezaur Rahman <os-rezaur.rahman@rakuten.com>
2023-12-14 12:11:02 +0600

    Global styles updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 
-import { HomePageComponent } from './home-page/home-page.component';
 import { CommonRoutingModule } from './common-routing.module';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [HomePageComponent],
