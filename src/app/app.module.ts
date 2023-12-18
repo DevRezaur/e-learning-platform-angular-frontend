9ccfbef6f93b5475ec6fbe8fcdbecdd6df2f761d
Rezaur Rahman <os-rezaur.rahman@rakuten.com>
2023-12-18 12:17:07 +0600

    Added resource server URI in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { OAuthModule } from 'angular-oauth2-oidc';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [AppRoutingModule, SharedModule],
+  imports: [
+    AppRoutingModule,
+    SharedModule,
+    HttpClientModule,
+    OAuthModule.forRoot({
+      resourceServer: {
+        allowedUrls: ['http://localhost:9090'],
+        sendAccessToken: true,
+      },
+    }),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
