578b7d7c7fa92143b107d8fb4f962eb7f8d8d3bf
Rezaur Rahman <rezaur.official@gmail.com>
2023-12-19 15:53:24 +0600

    Re-written auth service

--- src/app/shared/service/auth.service.ts
+++ src/app/shared/service/auth.service.ts
@@ -4,11 +4,9 @@ import {
   AuthConfig,
   NullValidationHandler,
   OAuthErrorEvent,
-  OAuthEvent,
   OAuthService,
   OAuthSuccessEvent,
 } from 'angular-oauth2-oidc';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -21,52 +19,45 @@ export class AuthService {
   public configure() {
     this.oauthService.configure(this.authConfig);
     this.oauthService.setupAutomaticSilentRefresh();
-    this.oauthService.tokenValidationHandler = new NullValidationHandler();
     this.oauthService.loadDiscoveryDocumentAndTryLogin();
+    this.oauthService.tokenValidationHandler = new NullValidationHandler();
   }
 
   private authConfig: AuthConfig = {
+    responseType: 'code',
+    clientId: 'spa-client',
+    scope: 'openid profile email offline_access',
     issuer: 'http://localhost:8080/realms/e-learning-platform',
     redirectUri: window.location.origin + '/common/callback',
     postLogoutRedirectUri: window.location.origin,
-    clientId: 'spa-client',
-    scope: 'openid profile email offline_access',
-    responseType: 'code',
     requireHttps: false,
     disableAtHashCheck: true,
     showDebugInformation: true,
   };
 
+  public login() {
+    this.oauthService.initCodeFlow();
+  }
+
+  public logout() {
+    this.oauthService.logOut();
+  }
+
   public redirectOnCallback(): void {
     this.oauthService.events.subscribe((event) => {
-      if (event instanceof OAuthErrorEvent) {
-        console.error(event);
-      } else if (event instanceof OAuthSuccessEvent) {
-        if (
-          event.type === 'token_received' &&
-          window.location.href === 'http://localhost:4200/common/callback'
-        ) {
-          if (this.isAdmin()) {
-            this.router.navigate(['/common/home']);
-          } else if (this.isUser()) {
-            this.router.navigate(['/common/home']);
-          }
+      if (
+        event instanceof OAuthSuccessEvent &&
+        event.type === 'token_received'
+      ) {
+        if (this.isAdmin()) {
+          this.router.navigate(['/common/home']);
+        } else if (this.isUser()) {
+          this.router.navigate(['/common/home']);
         }
-      } else {
-        console.warn(event);
+      } else if (event instanceof OAuthErrorEvent) {
+        console.error(event);
       }
     });
-  }
-
-  public getClaims(): string[] {
-    const accessToken: string = this.oauthService.getAccessToken();
-    const tokens: string[] = accessToken.split('.');
-    const claims = JSON.parse(atob(tokens[1]));
-    return claims.realm_access.roles;
-  }
-
-  public getOAuthEvent(): Observable<OAuthEvent> {
-    return this.oauthService.events;
   }
 
   public isLoggedIn(): boolean {
@@ -81,36 +72,35 @@ export class AuthService {
     return this.getClaims().includes('USER') ? true : false;
   }
 
-  public get username() {
-    let claims: any = this.oauthService.getIdentityClaims();
-    return claims ? claims.preferred_username : '';
+  public getClaims(): string[] {
+    const accessToken: string = this.oauthService.getAccessToken();
+    const splittedToken: string[] = accessToken.split('.');
+    const claims = JSON.parse(atob(splittedToken[1]));
+    return claims.realm_access.roles;
   }
 
-  public get userId() {
+  public getUserId(): string {
     let claims: any = this.oauthService.getIdentityClaims();
     return claims ? claims.sub : '';
   }
 
-  public get idToken() {
-    let token: any = this.oauthService.getIdToken();
+  public getUsername(): string {
+    let claims: any = this.oauthService.getIdentityClaims();
+    return claims ? claims.preferred_username : '';
+  }
+
+  public getIdToken(): string {
+    let token: string = this.oauthService.getIdToken();
     return token ? token : '';
   }
 
-  public get accessToken() {
-    let token: any = this.oauthService.getAccessToken();
+  public getAccessToken(): string {
+    let token: string = this.oauthService.getAccessToken();
     return token ? token : '';
   }
 
-  public get refreshToken() {
-    let token: any = this.oauthService.getRefreshToken();
+  public getRefreshToken(): string {
+    let token: string = this.oauthService.getRefreshToken();
     return token ? token : '';
-  }
-
-  public login() {
-    this.oauthService.initCodeFlow();
-  }
-
-  public logout() {
-    this.oauthService.logOut();
   }
 }
