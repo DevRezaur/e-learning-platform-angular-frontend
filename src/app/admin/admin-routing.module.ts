9f8742183f4a35541cc80267d6861ace7ea948a8
Rezaur Rahman <os-rezaur.rahman@rakuten.com>
2024-02-19 13:58:40 +0600

    manage-course page routerLink added

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { ManageCoursePageComponent } from './manage-course-page/manage-course-page.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
+  },
+  {
+    path: 'manage-course',
+    component: ManageCoursePageComponent,
   },
   {
     path: 'general',
