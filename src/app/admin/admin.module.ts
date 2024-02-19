826fd37649eb59b28729dd96d662dd3d269ae9af
Rezaur Rahman <os-rezaur.rahman@rakuten.com>
2024-02-19 13:55:13 +0600

    manage-course-page component created

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { ManageCoursePageComponent } from './manage-course-page/manage-course-page.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, ManageCoursePageComponent],
   imports: [AdminRoutingModule],
 })
 export class AdminModule {}
---
+++ src/app/admin/manage-course-page/manage-course-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-course-page',
+  templateUrl: './manage-course-page.component.html',
+  styleUrls: ['./manage-course-page.component.scss'],
+})
+export class ManageCoursePageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
