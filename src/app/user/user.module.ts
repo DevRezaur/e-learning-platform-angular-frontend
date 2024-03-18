9e9a97ffad51f8d49b64c66655e9c6501add8c9f
Rezaur Rahman <rezaur.official@gmail.com>
2024-03-18 19:30:27 +0600

    Added validations in payment information form

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { UserRoutingModule } from './user-routing.module';
 import { CoursePurchasePageComponent } from './page/course-purchase-page/course-purchase-page.component';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CoursePurchasePageComponent],
-  imports: [CommonModule, UserRoutingModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, UserRoutingModule, ReactiveFormsModule],
 })
 export class UserModule {}
