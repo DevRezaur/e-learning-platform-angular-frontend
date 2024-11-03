e1d78353cc39cd0cf3829586e3c3a3ec0d4d2fe1
Rezaur Rahman <rezaur.official@gmail.com>
2024-11-03 19:30:06 +0600

    login-page styles added

--- src/app/general/page/login-page/login-page.component.ts
+++ src/app/general/page/login-page/login-page.component.ts
@@ -1,12 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonService } from 'src/app/shared/service/common.service';
+import { PopNotificationService } from 'src/app/shared/service/pop-notification.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent implements OnInit {
-  constructor() {}
+export class LoginPageComponent {
+  loginDataForm: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(
+    private commonService: CommonService,
+    private popNotificationService: PopNotificationService,
+    private formBuilder: FormBuilder
+  ) {
+    this.loginDataForm = this.formBuilder.group({
+      email: ['', Validators.required, Validators.email],
+      password: ['', Validators.required],
+    });
+  }
+
+  login(): void {
+    this.commonService.markFormGroupTouched(this.loginDataForm);
+    if (!this.loginDataForm.valid) return;
+  }
+
+  private handleLoginSuccess(response: any): void {}
+
+  private handleLoginError(response: any): void {
+    const message = response.error.errorBody.errorMessage;
+    this.popNotificationService.setMessage(message);
+  }
 }
